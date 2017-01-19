610a11cf70d763a56a9ba55b8d3094ec02580a1f
rockchen26sh <cck8787878@gmail.com>
2017-01-19 13:56:12 +0800

    0119

--- 3.py
+++ 3.py
@@ -7,7 +7,7 @@ Created on Wed Jan 18 08:51:14 2017
 import pandas as pd
 import numpy as np
 #读取1880出生数据
-names_1880 = pd.read_csv("names/yob1880.txt",
+names_1880 = pd.read_csv("ch02/names/yob1880.txt",
                          header=None,
                          names = ['name','sex','births'])
 #按性别分组计算出生人数
@@ -19,7 +19,7 @@ col = ['name','sex','births']
 total_names = []
 
 for year in years:
-    path = "names/yob" + str(year) + '.txt'
+    path = "ch02/names/yob" + str(year) + '.txt'
     frame = pd.read_csv(path,header=None,names = col)
     frame['year'] = year
     total_names.append(frame)
@@ -58,8 +58,59 @@ total_births = top1000.pivot_table(index='year',
                                    values='births',
                                    aggfunc=sum)
 total_births
-subset = total_births[['Amanda','Amy','Andrew','Ashley']]
-subset.plot(subplots=False,figsize=(12,5),grid=True,
+subset = total_births[['William','Amy','Andrew','Ashley']]
+#四个name趋势图
+subset.plot(subplots=True,figsize=(12,15),grid=True,
             title='Numer of births per year')
 
+table = top1000.pivot_table('prop',index=['year'],columns='sex',aggfunc=sum)
+table.plot(title='sum of table1000.prop by year and sex',figsize=(10,5),
+           yticks=np.linspace(0,1.0,11),xticks=range(1880,2020,10))
 
+prop_cumsum = total_names.sort_index(by = 'prop',ascending=False).prop.cumsum()
+prop_cumsum[:10]
+prop_cumsum.searchsorted(0.5)
+df = boys[boys.year == 1990 ]
+df = df.sort_index(by='prop',ascending =False)
+
+in1900 = df.sort_index(by='prop',ascending = False).prop.cumsum()
+in1990 = in1900.searchsorted(0.5)+1
+
+def get_quantile_count(group,q=0.5):
+    group = group.sort_index(by='prop',ascending=False)
+    return group.prop.cumsum().searchsorted(q)+1
+
+diversity = top1000.groupby(['year','sex']).apply(get_quantile_count)
+diversity = diversity.unstack('sex')
+diversity.plot(title='Numblr of popular names in top 50%')
+
+get_last_letter = lambda x : x[-1]
+total_names['last'] = total_names.name.map(get_last_letter)
+total_names[:5]
+table = total_names.pivot_table('births',index = 'last' ,
+                                columns=['sex','year'],aggfunc=sum)
+subtable = table.reindex(columns=[1910,1960,2010],level='year')
+subtable
+subtable.sum()
+letter_prop = subtable / subtable.sum().astype(float)
+letter_prop
+import matplotlib.pyplot as plt
+fig,axes = plt.subplots(2,1,figsize = (10,8))
+letter_prop['M'].plot(kind='bar',rot=0,ax=axes[0],title='Male')
+letter_prop['F'].plot(kind='bar',rot=0,ax=axes[1],title='Female',legend=False)
+
+letter_prop = table /table.sum().astype(float)
+dny_ts = letter_prop.ix[['d','n','y'],'M'].T
+dny_ts.head()
+dny_ts.plot()
+
+all_names = total_names.name.unique()
+all_names
+mask = np.array(['lesl' in x.lower() for x in all_names])
+lesley_like = all_names[mask]
+lesley_like
+filtered = total_names[total_names.name.isin(lesley_like)]
+filtered.groupby('name').births.sum()
+table = filtered.pivot_table('births',index='year',columns = 'sex',
+                             aggfunc = sum)
+table.plot(style={'M':'k-','F':'k--'},figsize=(10,5))
---
+++ 4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 11:18:46 2017
+
+@author: chenshengkang
+"""
+
