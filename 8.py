48eb7c4dda29b9e66c7921eafe0e8ceb822323a3
rockchen26sh <cck8787878@gmail.com>
2017-03-07 20:43:04 +0800

    0307

--- 8.py
+++ 8.py
@@ -9,12 +9,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sc
 from pylab import *
+
+plot(np.arange(10))
 fig = plt.figure()
-ax1 = fig.add_subplots(2,2,1)
-ax2 = fig.add_subplots(2,2,2)
-ax3 = fig.add_subplots(2,2,3)
+
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
 from numpy.random import randn
 plt.plot(randn(50).cumsum(), 'k--')
+
+
+
 _ = ax1.hist(randn(100),bins = 20,color = 'k' , alpha=0.3)
 ax2.scatter(np.arange(30),np.arange(30)+3 * randn(30))
 
